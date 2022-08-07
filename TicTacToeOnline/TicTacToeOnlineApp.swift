41aecd3b76143ee0ffc50cf588577c53d9496593
atakanapan <atakanapan12@gmail.com>
2022-08-07 17:13:28 +0300

    Initial Commit

---
+++ TicTacToeOnline/TicTacToeOnlineApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeOnlineApp.swift
+//  TicTacToeOnline
+//
+//  Created by Atakan Apan on 8/7/22.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeOnlineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
